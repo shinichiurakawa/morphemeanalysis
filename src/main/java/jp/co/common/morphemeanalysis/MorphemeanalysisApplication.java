92d1c4a0b384f14e56a8a5ee3b2420334b686b39
shinichiurakawa <shinichi.urakawa@gmail.com>
2018-10-13 20:50:56 +0900

    first commit

---
+++ src/main/java/jp/co/common/morphemeanalysis/MorphemeanalysisApplication.java
@@ -0,0 +1,12 @@
+package jp.co.common.morphemeanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorphemeanalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorphemeanalysisApplication.class, args);
+	}
+}
---
+++ src/test/java/jp/co/common/morphemeanalysis/MorphemeanalysisApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.co.common.morphemeanalysis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MorphemeanalysisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
